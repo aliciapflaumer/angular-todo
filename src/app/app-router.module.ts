8345a786e6edf6a612b02367b2654682c022b8b2
aliciapflaumer <alpfla7@gmail.com>
2018-09-30 19:56:13 -0400

    Create Tasks component
    - Used `ng generate component tasks --skip-import`
    and moved tasks from homepage

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 const Routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomepageComponent }
+  { path: 'home', component: HomepageComponent },
+  { path: 'tasks', component: TasksComponent }
 ];
 
 @NgModule({
