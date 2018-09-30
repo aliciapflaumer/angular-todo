8ee379019829e0b14197f44b71749fb705a85e0e
aliciapflaumer <alpfla7@gmail.com>
2018-09-30 18:53:49 -0400

    Add app router module
    - Add redirectTo route to `home`
    - Moved Navigation menu to app.component so that it is on every page
    of application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+// import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
-const appRoutes: Routes = [
+import { AppRouterModule } from './app-router.module';
 
-];
+// const appRoutes: Routes = [
+//   { path: '', redirectTo: '/home', pathMatch: 'full' },
+//   { path: 'home', component: HomepageComponent }
+// ];
 
 @NgModule({
   declarations: [
@@ -16,10 +19,11 @@ const appRoutes: Routes = [
     HomepageComponent
   ],
   imports: [
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true }
-    ),
+    // RouterModule.forRoot(
+    //   appRoutes,
+    //   { enableTracing: true }
+    // ),
+    AppRouterModule,
     BrowserModule,
     HttpModule,
   ],
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
